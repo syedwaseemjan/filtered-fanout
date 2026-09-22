19c3e3f94639db90ac7b104bb25610ae58a9492f
Waseem Jan <syedwaseemjan@gmail.com>
2026-09-22 15:43:21 +0500

    Import the CDK services the fan-out will own: topic, queue, alarm, and Lambda.

--- src/filtered_fanout/filtered_fanout.py
+++ src/filtered_fanout/filtered_fanout.py
@@ -6,3 +6,11 @@ from enum import Enum
 from typing import Mapping, Sequence
 
 from aws_cdk import Duration
+from aws_cdk import aws_cloudwatch as cloudwatch
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_lambda as lambda_
+from aws_cdk import aws_lambda_event_sources as lambda_event_sources
+from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as subscriptions
+from aws_cdk import aws_sqs as sqs
+from constructs import Construct
