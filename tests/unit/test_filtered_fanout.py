3427500ee786e645c5c9afaeb78dd1d0c3d6c1dd
Waseem Jan <syedwaseemjan@gmail.com>
2026-09-22 15:43:21 +0500

    Split queues with a redrive policy from the dead-letter queues that have none.

--- tests/unit/test_filtered_fanout.py
+++ tests/unit/test_filtered_fanout.py
@@ -22,3 +22,16 @@ def _stack():
         filter={"signal_type": ["Tubing Pressure"]},
     )
     return stack, fanout, lost, setpoints
+
+
+def _queues(template: assertions.Template):
+    queues = template.find_resources("AWS::SQS::Queue")
+    consumers = {}
+    dead_letters = {}
+    for logical_id, resource in queues.items():
+        props = resource.get("Properties", {})
+        if "RedrivePolicy" in props:
+            consumers[logical_id] = props
+        else:
+            dead_letters[logical_id] = props
+    return consumers, dead_letters
